31997204f91fafdd9f53a535bba084faedca36e0
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-11-15 11:44:18 -0300

    adding code

---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Carlos Amaral on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
